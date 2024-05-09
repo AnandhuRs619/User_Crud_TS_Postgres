267f87fc2076265971ed9d4e8f9f770b91405312
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-10 00:34:29 +0530

    convert into async await and add error handling using try catch

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import * as express from "express"
 import * as bodyParser from "body-parser"
-import { Request, Response } from "express"
+import { Request, Response, NextFunction } from 'express';
 import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
 import { User } from "./entity/User"
+
+
 
 AppDataSource.initialize().then(async () => {
 
@@ -13,16 +15,15 @@ AppDataSource.initialize().then(async () => {
 
     // register express routes from defined application routes
     Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next)
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result)
-            }
-        })
-    })
+        try {
+            (app as any)[route.method](route.route, async (req: Request, res: Response, next: NextFunction) => {
+                const result = await (new (route.controller as any))[route.action](req, res, next);
+                res.json(result);
+            });
+        } catch (error) {
+            next(error);
+        }
+    });
 
     // setup express app here
     // ...
@@ -31,22 +32,12 @@ AppDataSource.initialize().then(async () => {
     app.listen(3000)
 
     // insert new users for test
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Timber",
-            lastName: "Saw",
-            age: 27
-        })
-    )
 
-    await AppDataSource.manager.save(
-        AppDataSource.manager.create(User, {
-            firstName: "Phantom",
-            lastName: "Assassin",
-            age: 24
-        })
-    )
 
     console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
 
 }).catch(error => console.log(error))
+function next(error: any) {
+    throw new Error("Function not implemented.");
+}
+
