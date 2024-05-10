8d45f42401914edb73e1c73a3679578e06fd0614
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-11 00:31:03 +0530

    setting up Validation using new npm Express-validate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,58 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as morgan from "morgan";
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
 import { User } from "./entity/User";
 import { port } from "./config";
+import { validationResult } from "express-validator";
 
 function handleError(err, _req, res, _next) {
-    res.status(err.statusCode || 500).send({message:err.message});
+  res.status(err.statusCode || 500).send({ message: err.message });
 }
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
     try {
-        // create express app
-        const app = express();
-        app.use(morgan("tiny"))
-        app.use(bodyParser.json());
+      // create express app
+      const app = express();
+      app.use(morgan("tiny"));
+      app.use(bodyParser.json());
 
-        // register express routes from defined application routes
-        Routes.forEach(route => {
-            (app as any)[route.method](route.route, async (req: Request, res: Response, next: NextFunction) => {
-                try {
-                    const result = await (new (route.controller as any))[route.action](req, res, next);
-                    res.json(result);
-                } catch (error) {
-                    next(error); // Pass the error to the next middleware (error handling middleware)
-                }
-            });
-        });
+      // register express routes from defined application routes
+      Routes.forEach((route) => {
+        (app as any)[route.method](
+          route.route,
+          ...route.validation,
+          async (req: Request, res: Response, next: NextFunction) => {
+            try {
+              const errors = validationResult(req);
+              if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+              }
+              const result = await new (route.controller as any)()[
+                route.action
+              ](req, res, next);
+              res.json(result);
+            } catch (error) {
+              next(error); // Pass the error to the next middleware (error handling middleware)
+            }
+          }
+        );
+      });
 
-        // error handler middleware
-        app.use(handleError);
+      // error handler middleware
+      app.use(handleError);
 
-        // start express server
-        app.listen(port, () => {
-            console.log(`Express server has started on port${port} . Open http://localhost:${port}/users to see results`);
-        });
+      // start express server
+      app.listen(port, () => {
+        console.log(
+          `Express server has started on port${port} . Open http://localhost:${port}/users to see results`
+        );
+      });
     } catch (error) {
-        console.error("An error occurred during initialization:", error);
+      console.error("An error occurred during initialization:", error);
     }
-}).catch(error => console.log(error));
+  })
+  .catch((error) => console.log(error));
