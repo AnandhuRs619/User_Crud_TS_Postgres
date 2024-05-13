5e471ace316006c027907313b4c0db892e521f64
AnandhuRs619 <ananthurs619@gmail.com>
2024-05-13 23:58:39 +0530

    create app and npm jest for testing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,54 +1,14 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import * as morgan from "morgan";
-import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "./data-source";
-import { Routes } from "./routes";
-import { User } from "./entity/User";
 import { port } from "./config";
-import { validationResult } from "express-validator";
-
-function handleError(err, _req, res, _next) {
-  res.status(err.statusCode || 500).send({ message: err.message });
-}
+import app from './app';
 
 AppDataSource.initialize()
   .then(async () => {
     try {
-      // create express app
-      const app = express();
-      app.use(morgan("tiny"));
-      app.use(bodyParser.json());
-
-      // register express routes from defined application routes
-      Routes.forEach((route) => {
-        (app as any)[route.method](
-          route.route,
-          ...route.validation,
-          async (req: Request, res: Response, next: NextFunction) => {
-            try {
-              const errors = validationResult(req);
-              if (!errors.isEmpty()) {
-                return res.status(400).json({ errors: errors.array() });
-              }
-              const result = await new (route.controller as any)()[
-                route.action
-              ](req, res, next);
-              res.json(result);
-            } catch (error) {
-              next(error); // Pass the error to the next middleware (error handling middleware)
-            }
-          }
-        );
-      });
-
-      // error handler middleware
-      app.use(handleError);
-
       // start express server
       app.listen(port, () => {
         console.log(
-          `Express server has started on port${port} . Open http://localhost:${port}/users to see results`
+          `Express server has started on port ${port}. Open http://localhost:${port}/users to see results`
         );
       });
     } catch (error) {
